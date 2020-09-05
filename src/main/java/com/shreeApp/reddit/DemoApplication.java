5e9b54cf92b797018918b2d9eaf7f31ebd162052
Shreesh Pandey <er.shreeshpandey@gmail.com>
2020-09-05 23:18:37 +0530

    swagger api added successfully!

--- src/main/java/com/shreeApp/reddit/DemoApplication.java
+++ src/main/java/com/shreeApp/reddit/DemoApplication.java
@@ -2,12 +2,14 @@ package com.shreeApp.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import com.shreeApp.reddit.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
-//@ComponentScan({"com.shreeApp.reddit.mapper.*"})
+@Import(SwaggerConfiguration.class)
 public class DemoApplication {
 
 	public static void main(String[] args) {
